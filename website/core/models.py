8543c1dcdde5c07a5a830edf479f5937c8fe3172
karolow <karol.piekarski@gmail.com>
2020-10-16 14:19:16 +0200

    update core abstract models

--- website/core/models.py
+++ website/core/models.py
@@ -15,10 +15,10 @@ class PersistentItem(models.Model):
         abstract = True
 
 
-class Entry(models.Model):
+class Entry(PersistentItem):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=5000, blank=True)
-    location = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, blank=True)
     room = models.CharField(max_length=255, blank=True)
 
     class Meta:
@@ -29,10 +29,52 @@ class Entry(models.Model):
 
 
 class ContactDetails(models.Model):
-    website = models.URLField()
-    email = models.EmailField()
+    website = models.URLField(blank=True)
+    email = models.EmailField(blank=True)
     phone = models.CharField(max_length=50, blank=True)
     contact_person = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Ownership(models.Model):
+    OWN = 'o'
+    COLLABORATION = 'c'
+    EXTERNAL = 'e'
+
+    OWNERSHIP_CHOICES = [
+        (OWN, 'Own'),
+        (COLLABORATION, 'Collaboration'),
+        (EXTERNAL, 'External'),
+    ]
+
+    ownership = models.CharField(
+        max_length=1,
+        choices=OWNERSHIP_CHOICES,
+        default=OWN,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Status(models.Model):
+    PUBLISHED = 'p'
+    DRAFT = 'd'
+    CANCELLED = 'c'
+
+    STATUS_CHOICES = (
+        (PUBLISHED, 'Published'),
+        (DRAFT, 'Draft'),
+        (CANCELLED, 'Cancelled'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default=PUBLISHED,
+    )
 
     class Meta:
         abstract = True
