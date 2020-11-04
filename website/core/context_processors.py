c2bcdffa8577ead6909d5b5fa086c133bf170e51
karolow <karol.piekarski@gmail.com>
2020-11-04 15:44:53 +0100

    add organization name to context processors

--- website/core/context_processors.py
+++ website/core/context_processors.py
@@ -5,5 +5,6 @@ def contact_email_context_processor(request):
     kwargs = {
         'contact_email': settings.CONTACT_EMAIL,
         'project_name': settings.PROJECT_NAME,
+        'organization_name': request.user.organization,
     }
     return kwargs
