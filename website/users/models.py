8177a13ab61047ca02542bf33864c73ecbc353a5
karolow <karol.piekarski@gmail.com>
2020-11-04 16:13:31 +0100

    add first and last name to user model

--- website/users/models.py
+++ website/users/models.py
@@ -10,6 +10,11 @@ class CustomUser(AbstractUser):
         on_delete=models.SET_NULL,
         null=True
     )
-    email = models.EmailField(blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50, blank=True)
     position = models.CharField(max_length=255, blank=True)
+
+    def get_full_name(self):
+        # return '{0} {1}'.format(self.first_name, self.last_name)
+        return f'{self.first_name} {self.last_name}'
