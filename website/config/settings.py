a39f824d03a34ab264554946edeb8ecb9b1b5a7c
karolow <karol.piekarski@gmail.com>
2020-11-09 16:04:07 +0100

    add heroku app doamin

--- website/config/settings.py
+++ website/config/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ktw-event-manager.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
