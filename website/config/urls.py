a3c3409254421bc06262179c9fbcf1c615db7728
karolow <karol.piekarski@gmail.com>
2020-11-09 14:29:03 +0100

    hide admin url

--- website/config/urls.py
+++ website/config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path('users/', include('allauth.urls')),
     path('temp/', include('pages.urls')),
     path('all/', include('organizations.urls')),
