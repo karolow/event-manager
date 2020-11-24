88e999e2e4fe667bbd834257f781ee4a574ebf03
karolow <karol.piekarski@gmail.com>
2020-11-24 10:01:43 +0100

    add throttling to rest api

--- website/config/settings.py
+++ website/config/settings.py
@@ -238,4 +238,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 1000,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '1000/day'
+    }
 }
