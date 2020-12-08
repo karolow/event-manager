fc34ea97832cd16042ddc6421a6a4c601a615ecf
karolow <karol.piekarski@gmail.com>
2020-12-08 11:22:49 +0100

    attach group permissions to views

--- website/organizations/views.py
+++ website/organizations/views.py
@@ -1,7 +1,7 @@
 from django_filters.views import FilterView
 from django_tables2.views import SingleTableMixin
 from django_tables2.export.views import ExportMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from projects.models import Event
 from projects.filters import EventFilter
@@ -9,10 +9,12 @@ from .tables import MajorEventTable
 
 
 class MajorTableView(LoginRequiredMixin,
+                     PermissionRequiredMixin,
                      ExportMixin,
                      SingleTableMixin,
                      FilterView):
     model = Event
+    permission_required = 'projects.view_event'
     table_class = MajorEventTable
     filterset_class = EventFilter
     template_name = 'major_event_table.html'
